260c24f295251e0a58f6fc954dae649b55ba2aea
kd Bromley <kd.bromley1@gmail.com>
2020-12-07 12:16:02 -0800

    itial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function formatQueryParams(params) {
+    //const queryItems
+    //return queryItems.join('')
+}
+
+function getParksList(query, maxResults=10) {
+    //const params
+    //const queryString - pass params to formatQParams
+    //const url
+    //fetch
+}
+
+function displayParksList(responseJson) {
+    //empty results
+    //for loop through response
+    //remove hidden class
+}
+
+function watchForm() {
+    //handle Submit
+    //get data from form
+    //pass data to getParksList
+}
+
+$(watchForm);
