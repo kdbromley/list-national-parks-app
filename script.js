454d7f4d487ddd10a7075b66112c847b20223961
kd Bromley <kd.bromley1@gmail.com>
2020-12-07 12:44:47 -0800

    API trigger success, no display

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function formatQueryParams(params) {
-    //const queryItems
-    //return queryItems.join('')
-}
-
-function getParksList(query, maxResults=10) {
-    //const params
-    //const queryString - pass params to formatQParams
-    //const url
-    //fetch
-}
+const apiKey = 'PQ7q59BDNRjuJthl0zQabCtFJmoP9lfgpchSdAeB';
+const searchURL = 'https://developer.nps.gov/api/v1/parks'
 
 function displayParksList(responseJson) {
     //empty results
@@ -16,10 +7,46 @@ function displayParksList(responseJson) {
     //remove hidden class
 }
 
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURICompnent(key)}=${encodeURICompnent(params[key])}`);
+    return queryItems.join('&');
+}
+
+function getParksList(state, maxResults=10) {
+    //const params
+    const params = {
+        api_key: apiKey,
+        stateCode: state,
+        limit: maxResults
+    };
+    //const queryString - pass params to formatQParams
+    const queryString = formatQueryParams(params);
+    //const url
+    const url = searchURL + '?' + queryString;
+    //fetch
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => console.log(responseJson))  //displayParksList(responseJson)
+        .catch(err => {
+            $('.js-error-message').text(`Uh oh! An error occured: ${err.message}`);
+        });
+}
+
+
+
 function watchForm() {
-    //handle Submit
-    //get data from form
-    //pass data to getParksList
+    $('.js-form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+        const maxResults = $('#js-max-results').val();
+        getParksList(searchTerm, maxResults);
+    })
 }
 
 $(watchForm);
