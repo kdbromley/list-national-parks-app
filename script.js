5524c341747223bc3ca7d57ecc04ce9031c96353
kd Bromley <kd.bromley1@gmail.com>
2020-12-07 16:45:19 -0800

    updates for multi state searching

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const apiKey = 'PQ7q59BDNRjuJthl0zQabCtFJmoP9lfgpchSdAeB';
 const searchURL = 'https://developer.nps.gov/api/v1/parks'
 
 function displayParksList(responseJson) {
-    console.log(responseJson)
+    console.log(responseJson);
+    $('.js-error-message').text('');
     $('.results-list').empty();
     for (let i = 0; i < responseJson.data.length; i++) {
         $('.results-list').append(`
-        <li><h4><a href=${responseJson.data[i].url}>${responseJson.data[i].fullName}</a></h4>
+        <li><h4><a href=${responseJson.data[i].url}>${responseJson.data[i].fullName}</a> - ${responseJson.data[i].states}</h4>
         <p>${responseJson.data[i].description}</p>
         <p>Visit this parks website: <a href=${responseJson.data[i].url}>${responseJson.data[i].url}</a></p>
         </li>`)
@@ -15,16 +16,18 @@ function displayParksList(responseJson) {
 }
 
 function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+    const queryItems = Object.keys(params.stateCode)
+        .map(key => `stateCode=${encodeURIComponent(params.stateCode[key])}`);
+    queryItems.push((Object.keys(params.strings).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params.strings[key])}`)).join('&')); 
     return queryItems.join('&');
 }
 
-function getParksList(state, maxResults=10) {
+function getParksList(states, maxResults=10) {
     const params = {
-        api_key: apiKey,
-        stateCode: state,
-        limit: maxResults
+        strings: {
+          api_key: apiKey,
+          limit: maxResults },
+       stateCode: states,
     };
     const queryString = formatQueryParams(params);
     const url = searchURL + '?' + queryString;
@@ -47,8 +50,9 @@ function watchForm() {
     $('.js-form').submit(event => {
         event.preventDefault();
         const searchTerm = $('#js-search-term').val();
+        const searchTermArray = searchTerm.split(',');
         const maxResults = $('#js-max-results').val();
-        getParksList(searchTerm, maxResults);
+        getParksList(searchTermArray, maxResults);
     })
 }
 
